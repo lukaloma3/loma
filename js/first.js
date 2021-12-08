406036fe48ec8bc3c5de3fc301bc79df4e4daee2
lukaloma3 <luka3lomadze@gmail.com>
2021-12-08 17:02:28 +0400

    js

---
+++ js/first.js
@@ -0,0 +1,25 @@
+let year, month, day;
+year = 2021;
+month = 12;
+day = "wednesday";
+
+const century = 21;
+
+var name = "luka";
+var num1 = 11;
+var num2 = 23;
+
+console.log(year, month, day, );
+console.log( century);
+console.log( name);
+
+
+document.querySelector(".ans").innerHTML = Number(num1) * Number(num2);
+document.querySelector(".ans2").innerHTML = Number(num1) + Number(num2);
+
+
+
+
+
+
+
