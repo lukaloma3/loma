375d4669a13d5fd342e751993d79214fc7487642
lukaloma3 <luka3lomadze@gmail.com>
2021-12-12 03:07:37 +0400

    js

---
+++ js/second.js
@@ -0,0 +1,42 @@
+
+let num =  555555555 ;
+let array =  ["luka", "lomadze"] ;
+let string =  "luka lomadze" ;
+let boo1 =  true ;
+let boo2 =  false ;
+let object =  {firstName:"luka", lastName:"lomadze" } ;
+let func =  function( )  {} ;
+
+console.log( typeof(num) );
+
+document.getElementById("text").innerHTML = string ;
+
+console.log(object);
+console.log(object.lastName);
+
+console.log(array);
+console.log(array[0]);
+
+console.log(array[0], object.lastName);
+
+console.log( 5 == 5 )
+
+console.log( object.lastName == array[1] )
+
+let p = Math.PI;
+console.log(p)
+
+let newdate = new Date
+console.log(newdate)
+console.log(newdate.getFullYear())
+
+
+
+
+
+
+
+
+
+
+
