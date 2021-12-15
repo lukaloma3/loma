e9df38a24c03f53c070c6205e7f791e074275a59
lukaloma3 <luka3lomadze@gmail.com>
2021-12-15 11:14:55 +0400

    second js

--- js/second.js
+++ js/second.js
@@ -1,37 +1,81 @@
+let text = "მე ვსწავლობ itაკადემიაში";
+
+console.log(text.slice(3, -13));
+
+console.log(text.replace("itაკადემიაში", "frontend" ));
+ 
+console.log(text.split(" "));
+
+console.log(text.includes("მე"));
+
+console.log(text.startsWith("მე"));
+
+console.log(text.toUpperCase());
+
+let text1 = "       მე ვსწავლობ itაკადემიაში         ";
+
+console.log(text1.trim());
+
+
+
+let h5 = "Card title";
+
+let p = "Some quick example text to build on the card title and make up the bulk of the card's content." ;
+
+let a = "Go somewhere" ;
+
+
+document.getElementById("row").innerHTML =  (
+
+    `
+    <div class="card col-lg-4" style="width: 18rem; margin:auto;">
+  <img src="https://cdn.jumeirah.com/-/mediadh/dh/hospitality/jumeirah/hotels/dubai/burj-al-arab-jumeirah/hero/burj-al-arab-profile-exterior_6-4_landscape/burj-al-arab-profile-exterior_6-4_landscape__portrait.jpg?w=600" class="card-img-top w-100" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${h5}</h5>
+    <p class="card-text">${p}</p>
+    <a href="#" class="btn btn-primary">${a}</a>
+  </div>
+</div>
+    `
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 let num =  555555555 ;
 let array =  ["luka", "lomadze"] ;
 let string =  "luka lomadze" ;
-let boo1 =  true ;
-let boo2 =  false ;
+let boo1 =  true ; let boo2 =  false ;
 let object =  {firstName:"luka", lastName:"lomadze" } ;
 let func =  function( )  {} ;
-
 console.log( typeof(num) );
-
 document.getElementById("text").innerHTML = string ;
-
 console.log(object);
 console.log(object.lastName);
-
-console.log(array);
+console.log(array); 
 console.log(array[0]);
-
-console.log(array[0], object.lastName);
-
+console.log(array[0], object.lastName); 
 console.log( 5 == 5 )
-
 console.log( object.lastName == array[1] )
-
-let p = Math.PI;
-console.log(p)
-
-let newdate = new Date
+let pi = Math.PI; console.log(p)
+let newdate = new Date ;
 console.log(newdate)
 console.log(newdate.getFullYear())
-
-
-
 
 
 
